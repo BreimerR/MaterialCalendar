f4a0a9bfb7fc28d587dd35ab532beb0714fe6c0a
Breimer <brymher@gmail.com>
2020-05-27 00:15:38 +0300

    pausing creation of material day view

--- library/src/main/java/com/gmail/brymher/materialcalendar/MaterialDayView.kt
+++ library/src/main/java/com/gmail/brymher/materialcalendar/MaterialDayView.kt
@@ -2,28 +2,38 @@ package com.gmail.brymher.materialcalendar
 
 import android.app.Service
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.min
 
+/**@description
+ * display selected day
+ * enable adding custom day view
+ * */
 class MaterialDayView : ViewGroup {
 
     var dayHeight = SIZE.px
     var dayWidth = SIZE.px
 
     val size get() = min(height, width)
-    val Int.px: Int
+
+    private val Int.px: Int
         get() {
             return TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), resources.displayMetrics
             ).toInt()
         }
 
+    @Suppress("MemberVisibilityCanBePrivate")
     val inflater = context.getSystemService(Service.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    val view = inflater.inflate(R.layout.day_view, this, false)
+    private val view = inflater.inflate(R.layout.day_view, this, false)
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -33,8 +43,35 @@ class MaterialDayView : ViewGroup {
         init(context, attrs)
     }
 
-    fun init(context: Context, attrs: AttributeSet? = null) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(context, attrs)
+    }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    ) {
+        init(context, attrs)
+    }
+
+    fun init(context: Context, attrs: AttributeSet? = null) {
+        initView()
+    }
+
+    fun initView() {
+        addView(view)
     }
 
     /**@description
@@ -42,7 +79,16 @@ class MaterialDayView : ViewGroup {
      * and not exactly to add views to the layout
      * */
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        for (i in 0 until childCount) {
 
+            val child = getChildAt(i)
+            if (child.visibility == View.GONE) continue
+
+            val width = child.measuredWidth
+            val height = child.measuredHeight
+
+
+        }
     }
 
     /**@description
@@ -57,5 +103,11 @@ class MaterialDayView : ViewGroup {
         var SIZE = 40
     }
 
+
+    abstract class OnClickListener {
+        abstract fun onClickAction()
+    }
+
+    abstract class OnLongPressListener
 
 }
